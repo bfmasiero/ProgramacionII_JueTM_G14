58efb71fed778acff4052fd2ab847e52aac1a127
lucioltedeschi <lucioltedeschi@gmail.com>
2025-06-19 09:39:50 -0300

    Update TestIntegrado.java

--- TP3_ProgramacionII_JueTM_G14/src/test/TestIntegrado.java
+++ TP3_ProgramacionII_JueTM_G14/src/test/TestIntegrado.java
@@ -8,28 +8,28 @@ public class TestIntegrado {
     public static void main(String[] args) {
         Grafo<Persona> grafo = new Grafo<>();
 
-        Persona ana = new Persona("Ana", 30);
+        Persona pedro = new Persona("Pedro", 30);
         Persona luis = new Persona("Luis", 25);
         Persona marta = new Persona("Marta", 40);
         Persona juan = new Persona("Juan", 35);
 
         //Agregar nodos
-        grafo.agregarNodo(ana);
+        grafo.agregarNodo(pedro);
         grafo.agregarNodo(luis);
         grafo.agregarNodo(marta);
         grafo.agregarNodo(juan);
 
         //Agregar aristas (no dirigido)
-        grafo.añadirArista(ana, luis);
-        grafo.añadirArista(ana, marta);
+        grafo.añadirArista(pedro, luis);
+        grafo.añadirArista(pedro, marta);
         grafo.añadirArista(marta, juan);
 
         //Matriz de adyacencia
         grafo.mostrarMatrizAdyacencia();
-        //Recorrido de BFS desde Ana
-        grafo.bfs(ana);
-        //Recorrido de DFS desde Ana
-        System.out.println("\nRecorrido DFS desde Ana:");
-        grafo.dfs(ana);
+        //Recorrido de BFS desde Pedro
+        grafo.bfs(pedro);
+        //Recorrido de DFS desde Pedro
+        System.out.println("Recorrido DFS desde Pedro:");
+        grafo.dfs(pedro);
     }
 }
